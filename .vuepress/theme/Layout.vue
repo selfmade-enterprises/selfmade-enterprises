3e1811a13b2f16ff33881df19828ec75ede66ca6
Manu <manuel.d.ketterer@googlemail.com>
2019-01-23 22:14:21 +0100

    Update coming soon

--- .vuepress/theme/Layout.vue
+++ .vuepress/theme/Layout.vue
@@ -6,20 +6,17 @@
                     <v-flex xs12 sm10 md8 class="text-xs-center">
                         <v-img src="" alt="logo"></v-img>
 
-                        <div class="amber--text text--darken-1 hidden-md-and-down display-4 font-weight-black text-uppercase">
+                        <div class="white--text display-3 font-weight-medium text-uppercase">
                             Coming soon
                         </div>
-                        <div class="amber--text text--darken-1 hidden-lg-and-up display-3 font-weight-black text-uppercase">
-                            Coming soon
-                        </div>
-                        <div class="display-3 font-weight-black">2019</div>
+                        <div class="display-2 font-weight-black gold">2019</div>
                     </v-flex>
                 </v-layout>
             </v-container>
         </v-content>
         <v-footer>
             <v-layout align-center justify-center>
-                <div class="justify-center">&copy;2019 - Selfmade Enterprises</div>
+                <div class="justify-center">&copy;2019 - SELF.MADE. Enterprises</div>
             </v-layout>
         </v-footer>
     </v-app>
@@ -27,4 +24,8 @@
 
 <style lang="stylus">
     @import '~vuetify/src/stylus/main'
+
+    .gold {
+        color: #AE9A64;
+    }
 </style>
